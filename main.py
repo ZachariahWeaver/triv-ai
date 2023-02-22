94399ebfa04f200d629b275596d621dd3bd5147b
Ziggy <zachariahjweaver@gmail.com>
2023-02-21 22:24:49 -0500

    Established connection to GCP CLoud SQL database

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
 from flask import Flask, render_template
+import pymysql
+from datetime import date
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+
+cnx = pymysql.connect(
+    host=config.get('database', 'MYSQL_HOST'),
+    user=config.get('database','MYSQL_USER'),
+    password=config.get('database','MYSQL_PASSWORD'),
+    database=config.get('database','MYSQL_DB'),
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+with cnx.cursor() as cursor:
+    sql = "SELECT * FROM categories"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+cnx.close()
+
+clues = [
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+]
+categories = ["", "", "", "", "", ""]
+prizes = [0, 200, 400, 600, 800, 1000]
+
 
 app = Flask(__name__)
 
 
-clues = [["clue 1-1", "clue 1-2", "clue 1-3", "clue 1-4", "clue 1-5", "clue 1-6"],
-         ["clue 2-1", "clue 2-2", "clue 2-3", "clue 2-4", "clue 2-5", "clue 2-6"],
-         ["clue 3-1", "clue 3-2", "clue 3-3", "clue 3-4", "clue 3-5", "clue 3-6"],
-         ["clue 4-1", "clue 4-2", "clue 4-3", "clue 4-4", "clue 4-5", "clue 4-6"],
-         ["clue 5-1", "clue 5-2", "clue 5-3", "clue 5-4", "clue 5-5", "clue 5-6"],
-         ];
-
-
 @app.route('/')
 def home():
-    # generate two 6x6 2D arrays of strings (placeholder text for now)
-    categories = ["Cat 1", "Cat 2", "Cat 3", "Cat 4", "Cat 5", "Cat 6"]
-    prizes = [0, 200, 400, 600, 800, 1000]
+    return render_template('home.html')
 
-    return render_template('index.html', prizes=prizes, clues=clues, categories=categories)
+
+@app.route('/daily')
+def daily():
+    code = date.today()
+    # generate two 6x6 2D arrays of strings (placeholder text for now)
+    return render_template('daily.html', prizes=prizes, clues=clues, categories=categories)
+
+
+@app.route('/archive/<int:clue_id>')
+def archive():
+    # generate two 6x6 2D arrays of strings (placeholder text for now)
+    # query database for questions from this day. if no database table exists
+    return render_template('daily.html', prizes=prizes, clues=clues, categories=categories, game_id=variable)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
+# TODOLIST: Fix clue validation and score keeping. Set up clue/category generation, setup
