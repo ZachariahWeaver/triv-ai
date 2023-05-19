fd43764b6bcff4d694e3bd843dd88c5dc0bf5b07
Ziggy <zachariahjweaver@gmail.com>
2023-05-19 00:21:34 -0400

    Refactored some of the database connections to set up for migrating to pythonanywhere host from GCP

--- main.py
+++ main.py
@@ -1,124 +1,58 @@
-import json
+import os
+import pymysql
+from flask import Flask, render_template, request
 
-from flask import Flask, render_template, redirect  # IMPORT Flask for the web
-import pymysql  # to connect to Google Cloud SQL Database (MySQL)
-from datetime import date  # For getting the date to keep clue sets daily
-from configparser import ConfigParser
-import openai  # to connect with Open AI's text generation
+import configparser
 
-config = ConfigParser()  # Take credentials etc. from config file to keep it private and connect to the folowing
+# Load configuration from the config.ini file
+config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Open AI
-# openai.api_key = config.get('openai', 'SECRET_KEY')
-
-# GCP Cloud SQL Database
-cnx = pymysql.connect(
-    host=config.get('database', 'MYSQL_HOST'),
-    user=config.get('database', 'MYSQL_USER'),
-    password=config.get('database', 'MYSQL_PASSWORD'),
-    database=config.get('database', 'MYSQL_DB'),
-    cursorclass=pymysql.cursors.DictCursor
-)
-
-PRIZE_VALUES = [200, 400, 600, 800, 1000]  # these prize numbers are consistent
-# GENERATE DAILY SET (GENERATE CATEGORY SET > GENERATE CLUE SET)
-# STORE DAILY SET (INSERT CATEGORY SET > INSERT CLUE SET)
-# RETRIEVE DAILY SET OR PREVIOUS SET
+database_credentials = 'local_database'     #change this variable to 'pythonanywhere' for deployment to PythonAnywhere 
+db_user = config.get(database_credentials, 'db_user')
+db_password = config.get(database_credentials, 'db_password')
+db_name = config.get(database_credentials, 'db_name')
+db_host = config.get(database_credentials, 'db_host')
 
 
+PRIZE_VALUES = [200, 400, 600, 800, 1000]
 
-
-# def generate_daily_set(day):
-#     insert_categories(generate_categories())
-#     insert_clues(generate_clues())
-#     #         engine=model,
-#     #         prompt=prompt,
-#     #         temperature=temperature,
-#     #         max_tokens=2048,
-#     #         n=1,
-#     #         stop=None,
-#     #         frequency_penalty=0,
-#     #         presence_penalty=0
-# def generate_categories():
-#     #     response = openai.Completion.create()
-#     pass
-# def generate_clues(prompt, model, temperature=0.5):
-#     #     response = openai.Completion.create()
-#    pass
-# def insert_categories():
-#     pass
-# def insert_clues(inserts, category_ids):
-#     with cnx.cursor() as cursor:
-#         for column_index, num in enumerate(category_ids):
-#             for sql_index, query in enumerate(inserts):
-#                 sql = "INSERT INTO PromptsSchema.clues(clue, response, category_id, ranking) VALUES ('" + str(
-#                     query) + "', '" + str(inserts[query]) + "', " + num + ", " + str(sql_index + 1) + ");"
-#                 print(sql)
-#                 cursor.execute(sql)
-#         cnx.commit()
-#         cnx.close()
-#
-# def get_daily_set(day):
-#     sql = "SELECT categories.title, clues.clue, clues.response FROM categories INNER JOIN clues ON categories.id = clues.category_id WHERE categories.creation_date = '" + day + "' ORDER BY categories.ranking, clues.ranking;"
-#     with cnx.cursor() as cursor:
-#         cursor.execute(sql)
-#         result = cursor.fetchall()
-#         print(result)
 app = Flask(__name__)
 
 @app.route('/')
 def daily():
-    today = '2023-02-22';
+    today = '2023-02-22'
     sql = "SELECT categories.title, GROUP_CONCAT(clues.clue SEPARATOR '@@@') AS clues, GROUP_CONCAT(clues.response " \
           "SEPARATOR '@@@') AS responses FROM categories LEFT JOIN clues ON categories.id = clues.category_id WHERE " \
           "categories.creation_date = '" + today + "' GROUP BY categories.title; "
+    cnx = pymysql.connect(user=db_user, password=db_password, host=db_host, db=db_name)
+
     with cnx.cursor() as cursor:
         cursor.execute(sql)
         result = cursor.fetchall()
 
-        categories = []
-        clues = []
-        responses = []
+    cnx.close()
 
-        for r in result:
-            categories.append(r['title'])
-            clues.append(r['clues'].split("@@@"))
-            responses.append(r['responses'].split("@@@"))
-        print(categories)
-        print(clues)
-        print(responses)
+    categories = []
+    clues = []
+    responses = []
+    print(result)
+    for r in result:
+        categories.append(r[0])
+        clues.append(r[1].split("@@@"))
+        responses.append(r[2].split("@@@"))
+    
+    print(categories)
+    print(clues)
+    print(responses)
+
     return render_template('index.html', prizes=PRIZE_VALUES, categories=categories, clues=clues, responses=responses)
-
 
 @app.route('/archive/<int:clue_set_date>')
 def archive(clue_set_date):
-    if clue_set_date > date.today() or clue_set_date > '2023-22-02':
-        redirect('/')
-    sql = "SELECT categories.title, GROUP_CONCAT(clues.clue SEPARATOR '@@@') AS clues, GROUP_CONCAT(clues.response " \
-          "SEPARATOR '@@@') AS responses FROM categories LEFT JOIN clues ON categories.id = clues.category_id WHERE " \
-          "categories.creation_date = '" + clue_set_date + "' GROUP BY categories.title; "
-    with cnx.cursor() as cursor:
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        categories = []
-        clues = []
-        responses = []
+    # You can implement this function similarly to the `daily()` function.
+    pass
 
-        for r in result:
-            categories.append(r['title'])
-            clues.append(r['clues'].split("@@@"))
-            responses.append(r['responses'].split("@@@"))
-    return render_template('index.html', prizes=PRIZE_VALUES, categories=categories, clues=clues, responses=responses)
 
-#     if clue_set_date > date.today():
-#         return redirect('/')
-#
-# today = clue_set_date category_ids = get_category_ids(today) # ideally fix this so it returns one object and the
-# parameters below get passed values from the object
-#
-# return render_template('index.html', prizes=PRIZE_VALUES, clues=get_clues(category_ids), categories=get_categories(
-# category_ids))
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+if __name__ == '__main__':  #remove these lines for deployment to PythonAnywhere 
+    app.run(debug=True, use_reloader=False) 
--- static/script.js
+++ static/script.js
@@ -1,94 +1,100 @@
-    let colCount = [5,5,5,5,5,5];
-    let titles = document.getElementsByClassName("category-boxes");
-    let clueCount = 30;
-    let score = 0;
-    let values = [200, 400, 600, 800, 1000];
-    let timeLeft = 10;
-    let timer;
-    let buzzed = false;
+let colCount = [5,5,5,5,5,5];
+let titles = document.getElementsByClassName("category-boxes");
+let clueCount = 30;
+let score = 0;
+let values = [200, 400, 600, 800, 1000];
+let timer;
+let buzzed = false;
 
-function revealClue(row, col, clues) {
-    var clue = clues[row][col];
-    document.getElementById('activecluetext').innerText = clue;
-    var x = document.getElementById('activeclue');
-    x.style.display = 'flex';
-    startTimer();
-    // get the clicked clue box
-    var clickedBox = event.target;
-    // hide the clue box by setting its visibility property to "hidden"
-    clickedBox.innerText = "";  // remove the text content
-    clickedBox.removeAttribute("onclick");  // remove the onclick attribute
-    updateColumns(col);
-    score = score + values[row];
-    console.log(row);
-
+function revealClue(col, row, clues) {
+var clue = clues[col][row];
+document.getElementById('activecluetext').innerText = clue;
+var z = document.getElementById('activecluecontainer');
+z.style.display = 'flex';
+var y = document.getElementById('grid')
+y.style.display = 'none'
+startTimer();
+// get the clicked clue box
+var clickedBox = event.target;
+// hide the clue box by setting its visibility property to "hidden"
+clickedBox.innerText = "";  // remove the text content
+clickedBox.removeAttribute("onclick");  // remove the onclick attribute
+updateColumns(col);
+score = score + values[row];
+activeclue.setAttribute('onclick', 'buzzIn()' );
 }
 
 function hideClue(condition){
-  var x = document.getElementById('activeclue');
-    x.style.display = 'none'; //CHANGE SCORE BASED ON CONDITIONS
-    console.log(condition);
-    console.log(buzzed)
-    updateCategories();
+var z = document.getElementById('activecluecontainer');
+z.style.display = 'none';
+var y = document.getElementById('grid')
+y.style.display = 'grid'
+var x = document.getElementById('submissionbox')
+x.style.display = 'none'
+updateCategories();
+document.getElementById('activeclueinput').value = ''
 
 }
 
 function updateColumns(col){
 
 
-    console.log("column " + col + " is at " + colCount[col])
-    colCount[col] = colCount[col]-1;
-    if (colCount[col] === 0){
-        titles[col].innerText = "";  // remove the text content
-    }
+console.log("column " + col + " is at " + colCount[col])
+colCount[col] = colCount[col]-1;
+if (colCount[col] === 0){
+    titles[col].innerText = "";  // remove the text content
+}
 
 }
 
 function updateCategories(){
-    clueCount = clueCount - 1;
-    console.log(clueCount)
-    if (clueCount === 0){
-        document.getElementById('grid').style.display = 'none';
-        document.getElementById('endCard').style.display = 'flex';
-        document.getElementById("endgametext").innerHTML = "Game over <br>  SCORE: " + score;
-    }
+clueCount = clueCount - 1;
+console.log(clueCount)
+if (clueCount === 0){
+    document.getElementById('grid').style.display = 'none';
+    document.getElementById('endcardcontainer').style.display = 'flex';
+    document.getElementById("endgametext").innerHTML = "Game over <br>  SCORE: " + score;
+}
 }
 
 function submitAnswer() {
-    var answer = document.getElementById("activeclueinput").value;
-    console.log(answer);
-    stopTimer();
-    if(answer === answer){ // FIX VALIDATION
-        hideClue("right")
-    }
-    else{
-        hideClue("wrong")
-    }
+var answer = document.getElementById("activeclueinput").value;
+console.log(answer);
+stopTimer();
+if(answer === answer){ // FIX VALIDATION
+    hideClue("right")
+}
+else{
+    hideClue("wrong")
+}
 }
 
 function startTimer() {
-    buzzed = false;
-  document.getElementById("timer").innerHTML = "Time remaining: " + timeLeft + " seconds";
-  timer = setInterval(function() {
-    timeLeft--;
-    console.log(timeLeft);
-    document.getElementById("timer").innerHTML = "Time remaining: " + timeLeft + " seconds";
-    if (timeLeft <= 0) {
-      clearInterval(timer);
-      document.getElementById("timer").innerHTML = "Time's up!";
-      hideClue("timeup");
+buzzed = false;
+timeLeft = 10;
+document.getElementById("timer").innerHTML = timeLeft;
+timer = setInterval(function() {
+timeLeft--;
+console.log(timeLeft);
+document.getElementById("timer").innerHTML = timeLeft;
+if (timeLeft <= 0) {
+  clearInterval(timer);
+  document.getElementById("timer").innerHTML = "Time's up!";
+  hideClue("timeup");
 
-    }
-  }, 1000);
+}
+}, 1000);
 }
 function stopTimer() {
-  clearInterval(timer);
+clearInterval(timer);
 }
 
 function buzzIn(){
-        buzzed = true;
-        activeclue.removeAttribute("onclick");  // remove the onclick attribute
-        document.getElementById('submissionbox').style.display = "flex"
-        timeLeft = timeLeft + 10;
+    timeLeft = 10;
+    activeclue.removeAttribute('onclick');  // remove the onclick attribute
+    document.getElementById('submissionbox').style.display = "block"
 }
 
+function nothing(){
+    1+1;
+}
